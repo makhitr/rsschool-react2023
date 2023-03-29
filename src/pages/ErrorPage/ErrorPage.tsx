2a6da8d3e7797fddb278fd14c13f4e2ee6cff908
makhitr <ring_ru@yahoo.com>
2023-03-30 00:34:49 +0300

    refactor: pages with hooks

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-class ErrorPage extends React.Component {
-  render() {
-    return (
-      <div data-testid="error-page">
-        <h2>Error Page</h2>
-      </div>
-    );
-  }
-}
+const ErrorPage = () => {
+  return (
+    <div data-testid="error-page">
+      <h2>Error Page</h2>
+    </div>
+  );
+};
 
 export default ErrorPage;
