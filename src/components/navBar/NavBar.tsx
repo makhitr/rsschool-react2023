783fad38b626e10f4b12bf67991ccc67cd5662c8
makhitr <ring_ru@yahoo.com>
2023-03-31 20:32:34 +0300

    refactor: navbar with hooks

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,41 +1,39 @@
-import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './NavBar.module.css';
 
-class NavBar extends React.Component {
-  render() {
-    return (
-      <nav className={styles.navBar} data-testid="navigation">
-        <NavLink
-          to="/"
-          data-testid="main-link"
-          className={({ isActive }) => (isActive ? styles.navLinkActive : '')}
-        >
-          Main
-        </NavLink>
-        <NavLink
-          to="/about"
-          data-testid="about-link"
-          className={({ isActive }) => (isActive ? styles.navLinkActive : '')}
-        >
-          About us
-        </NavLink>
-        <NavLink
-          to="/form"
-          data-testid="from-link"
-          className={({ isActive }) => (isActive ? styles.navLinkActive : '')}
-        >
-          Form
-        </NavLink>
-        <NavLink
-          to="/*"
-          data-testid="error-link"
-          className={({ isActive }) => (isActive ? styles.navLinkActive : '')}
-        >
-          Error
-        </NavLink>
-      </nav>
-    );
-  }
-}
+const NavBar = () => {
+  return (
+    <nav className={styles.navBar} data-testid="navigation">
+      <NavLink
+        to="/"
+        data-testid="main-link"
+        className={({ isActive }) => (isActive ? styles.navLinkActive : '')}
+      >
+        Main
+      </NavLink>
+      <NavLink
+        to="/about"
+        data-testid="about-link"
+        className={({ isActive }) => (isActive ? styles.navLinkActive : '')}
+      >
+        About us
+      </NavLink>
+      <NavLink
+        to="/form"
+        data-testid="from-link"
+        className={({ isActive }) => (isActive ? styles.navLinkActive : '')}
+      >
+        Form
+      </NavLink>
+      <NavLink
+        to="/*"
+        data-testid="error-link"
+        className={({ isActive }) => (isActive ? styles.navLinkActive : '')}
+      >
+        Error
+      </NavLink>
+    </nav>
+  );
+};
+
 export default NavBar;
