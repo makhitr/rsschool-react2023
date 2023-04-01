1419533e723af091e03ba77e71f695e634e21612
makhitr <ring_ru@yahoo.com>
2023-04-01 16:48:37 +0300

    refactor: card, app functional components

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,13 @@ import AppRouter from './router/AppRouters';
 import NavBar from './components/navBar/NavBar';
 import styles from './App.module.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div data-testid="app" className={styles.app}>
-        <NavBar />
-        <AppRouter />
-      </div>
-    );
-  }
-}
+const App: React.FC = (): JSX.Element => {
+  return (
+    <div data-testid="app" className={styles.app}>
+      <NavBar />
+      <AppRouter />
+    </div>
+  );
+};
 
 export default App;
