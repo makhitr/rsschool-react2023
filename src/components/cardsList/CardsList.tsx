8b8678585881068854c54d3e3140614d18c55cb4
makhitr <ring_ru@yahoo.com>
2023-04-09 00:47:50 +0300

    feat: modal in portal

--- src/components/cardsList/CardsList.tsx
+++ src/components/cardsList/CardsList.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import Card from '../card/Card';
 import { CardsListProps } from '../../types';
+import Modal from '../../components/modal/Modal';
 import styles from './CardsList.module.css';
 
 const CardsList: React.FC<CardsListProps> = ({ cards }): JSX.Element => {
+  const [showModal, setShowModal] = React.useState(false);
+
   return (
-    <div className={styles.cardSection} data-testid="cards-list">
-      {cards.map((card, id) => (
-        <Card key={`card.name-${id}`} cardData={card} />
-      ))}
-    </div>
+    <>
+      <button onClick={() => setShowModal(true)}>show Modal</button>
+      {showModal && (
+        <Modal>
+          <div
+            className={styles.overlay}
+            onClick={(event) => {
+              if (event.currentTarget === event.target) {
+                setShowModal(false);
+              }
+              console.log(event);
+            }}
+          >
+            <div className={styles.cardModal}>
+              <h1>I am a card</h1>
+              <button onClick={() => setShowModal(false)}>close</button>
+            </div>
+          </div>
+        </Modal>
+      )}
+      <div className={styles.cardSection} data-testid="cards-list">
+        {cards.map((card, id) => (
+          <Card key={`card.name-${id}`} cardData={card} />
+        ))}
+      </div>
+    </>
   );
 };
 
